7b70815a71ac87190c716e4bc1904226ea84b285
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-11-08 03:28:33 -0800

    started scaler class

---
+++ scaler.cpp
@@ -0,0 +1,34 @@
+
+
+
+class Scaler
+{
+
+  Scaler(float value=0.0, grad=0.0, )
+  {
+     
+    float value = value;
+    float grad  = grad;
+    float backward
+    {
+      return 1.0;
+    }
+    void* _backward = backward; 
+  }
+   
+  Scaler operator +(Scaler const &other)
+  {
+    Scaler res;
+    res.value = value = a.value;
+    
+    float _backward()
+    {
+      this->grad = other.grad
+      other.grad = this->grad
+    }
+    this->_backward = _backward;
+    return res;
+  }
+
+
+};
